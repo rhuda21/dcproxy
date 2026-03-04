38efd0b4c26513bee7a174ce36f58bbe6a62f115
R H <165597973+rhuda21@users.noreply.github.com>
2026-03-04 21:51:55 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 export default {
   async fetch(request) {
     const url = new URL(request.url);
-    const match = url.pathname.match(/^\/api\/webhooks\/(\d+)\/([^\/]+)/);
-    return match 
-      ? await fetch(`https://discord.com/api/webhooks/${match[1]}/${match[2]}`, {
-          method: request.method,
-          headers: request.headers,
-          body: request.body
-        })
-      : new Response('Use: /api/webhooks/ID/TOKEN');
+    const webhookMatch = url.pathname.match(/^\/api\/webhooks\/(\d+)\/([^\/]+)/);
+    const apiMatch = url.pathname.match(/^\/discord(\/.*)?$/);
+
+    if (webhookMatch) {
+      return fetch(`https://discord.com/api/webhooks/${webhookMatch[1]}/${webhookMatch[2]}`, {
+        method: request.method,
+        headers: request.headers,
+        body: request.body
+      });
+    } else if (apiMatch) {
+      return fetch(`https://discord.com/api${apiMatch[1] || ""}${url.search}`, {
+        method: request.method,
+        headers: request.headers,
+        body: request.method !== "GET" ? request.body : null
+      });
+    }
+
+    return new Response("Use: /api/webhooks/ID/TOKEN or /discord/v10/...");
   }
 }
